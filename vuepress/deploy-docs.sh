689ef3a88eefd92e989682f26d12e33a7031017b
alexisflesch <alexis.flesch@gmail.com>
2025-07-03 23:11:23 +0200

    bug fix in doc deployment for assets

--- vuepress/deploy-docs.sh
+++ vuepress/deploy-docs.sh
@@ -9,8 +9,15 @@ REPO_URL="https://github.com/alexisflesch/mathquest.git"
 BRANCH="gh-pages"
 ORIGIN_DIR="$(pwd)"
 
-# Build la doc
+ # Build la doc
 npm run docs:build
+
+
+# Copie manuelle du logo et du favicon si besoin (contournement bug VuePress)
+cp "$(cd "$(dirname "$0")" && pwd)/docs/public/assets/logo.svg" "$BUILD_DIR/assets/logo.svg"
+if [ -f "$(cd "$(dirname "$0")" && pwd)/docs/public/favicon.ico" ]; then
+  cp "$(cd "$(dirname "$0")" && pwd)/docs/public/favicon.ico" "$BUILD_DIR/favicon.ico"
+fi
 
 cd "$BUILD_DIR"
 git init
--- vuepress/docs/.vuepress/.temp/internal/routes.js
+++ vuepress/docs/.vuepress/.temp/internal/routes.js
@@ -4,8 +4,8 @@ export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/home/aflesch/mathquest/vuepress/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
   ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"/home/aflesch/mathquest/vuepress/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
   ["/installation/", { loader: () => import(/* webpackChunkName: "installation_index.html" */"/home/aflesch/mathquest/vuepress/docs/.vuepress/.temp/pages/installation/index.html.js"), meta: {"title":"Installation"} }],
-  ["/utilisation/", { loader: () => import(/* webpackChunkName: "utilisation_index.html" */"/home/aflesch/mathquest/vuepress/docs/.vuepress/.temp/pages/utilisation/index.html.js"), meta: {"title":"Utilisation de l'application"} }],
   ["/questions-yaml/", { loader: () => import(/* webpackChunkName: "questions-yaml_index.html" */"/home/aflesch/mathquest/vuepress/docs/.vuepress/.temp/pages/questions-yaml/index.html.js"), meta: {"title":"Écriture de questions (YAML)"} }],
   ["/questions-yaml/contribuer.html", { loader: () => import(/* webpackChunkName: "questions-yaml_contribuer.html" */"/home/aflesch/mathquest/vuepress/docs/.vuepress/.temp/pages/questions-yaml/contribuer.html.js"), meta: {"title":"Contribuer à la base commune de questions"} }],
+  ["/utilisation/", { loader: () => import(/* webpackChunkName: "utilisation_index.html" */"/home/aflesch/mathquest/vuepress/docs/.vuepress/.temp/pages/utilisation/index.html.js"), meta: {"title":"Utilisation de l'application"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/home/aflesch/mathquest/vuepress/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
