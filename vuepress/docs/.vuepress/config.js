fcd22df04f674c4db81fe1ed562ab2a96f7cd4a7
alexisflesch <alexis.flesch@gmail.com>
2025-10-01 19:35:06 +0200

    removed "mathquest/" as baseurl now that it is on docs.kutsum.org

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -8,11 +8,11 @@ export default defineUserConfig({
   lang: 'fr-FR',
   title: 'MathQuest',
   description: 'Alternative libre et open source à Kahoot, pour les enseignants et leurs élèves.',
-  base: '/mathquest/',
+  base: '/',
 
   head: [
-    ['link', { rel: 'icon', href: '/mathquest/favicon.ico' }],
-    ['link', { rel: 'stylesheet', href: '/mathquest/styles/screenshots.css' }]
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'stylesheet', href: '/styles/screenshots.css' }]
   ],
 
   theme: defaultTheme({
